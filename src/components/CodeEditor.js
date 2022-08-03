45b3cb8257839dc34bdb43c09b55510accc268df
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2022-08-03 12:23:03 +1200

    Update CodeEditor.js

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -5,6 +5,20 @@ import {useColorMode} from '@docusaurus/theme-common';
 
 import "./CodeEditor.css"
 
+const editorOptions = {
+    enableBasicAutocompletion: true,
+    enableLiveAutocompletion: true,
+    highlightActiveLine: false,
+    showPrintMargin: false,
+    showGutter: false,
+};
+
+const editorOnLoad = editor => {
+    editor.renderer.setScrollMargin(10, 10, 0, 0);
+    editor.renderer.setPadding(16);
+    editor.moveCursorTo(0, 0);
+};
+
 export default function CodeEditor(props) {
     const [input, setInput] = useState(props.code.trimEnd());
 
@@ -20,42 +34,24 @@ export default function CodeEditor(props) {
         require("ace-builds/src-noconflict/ext-language_tools");
     }
 
-    return (
-        <BrowserOnly>
-            {() =>
-                <div className={"code-editor"}>
-                    <AceEditor
-                        value={input}
-                        mode="python"
-                        name="CodeBlock"
-                        fontSize={'0.9em'}
-                        theme={colorMode === 'dark' ? "idle_fingers" : "textmate"}
-                        onChange={(newValue, e) => {
-                            setInput(newValue)
-                        }}
-                        width='100%'
-                        maxLines={Infinity}
-                        style={{backgroundColor: "rgba(0, 0, 0, 0)"}}
-                        onLoad={editor => {
-                            editor.renderer.setScrollMargin(10, 10, 0, 0);
-                            editor.renderer.setPadding(16);
-                            editor.moveCursorTo(0, 0);
-                        }}
-                        editorProps={{$blockScrolling: true}}
-                        setOptions={{
-                            enableBasicAutocompletion: true,
-                            enableLiveAutocompletion: true,
-                            enableSnippets: true,
-                            highlightActiveLine: false,
-                            cursorStyle: "ace",
-                            showPrintMargin: false,
-                            scrollPastEnd: false,
-                            showLineNumbers: false,
-                            showGutter: false,
-                        }}
-                    />
-                </div>
-            }
-        </BrowserOnly>
-    )
+    return <BrowserOnly>
+        {() =>
+            <div className={"code-editor"}>
+                <AceEditor
+                    value={input}
+                    mode="python"
+                    name="CodeBlock"
+                    fontSize={'0.9em'}
+                    theme={colorMode === 'dark' ? "idle_fingers" : "textmate"}
+                    onChange={(newValue, e) => setInput(newValue)}
+                    width='100%'
+                    maxLines={Infinity}
+                    style={{backgroundColor: "rgba(0, 0, 0, 0)"}}
+                    onLoad={editorOnLoad}
+                    editorProps={{$blockScrolling: true}}
+                    setOptions={editorOptions}
+                />
+            </div>
+        }
+    </BrowserOnly>
 }
