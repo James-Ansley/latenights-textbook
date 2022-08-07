00d32297e0304d8a84dc5791aed37224465e1708
James Ansley <31982662+James-Ansley@users.noreply.github.com>
2022-08-07 19:13:39 +1200

    added runnable code windows

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -6,6 +6,9 @@ import {usePython} from "react-py";
 
 import "./CodeEditor.css"
 
+import {BsPlayFill} from "react-icons/bs";
+import {BiReset} from "react-icons/bi";
+
 const editorOptions = {
     enableBasicAutocompletion: true,
     enableLiveAutocompletion: true,
@@ -40,7 +43,10 @@ export default function CodeEditor(props) {
 
     return <BrowserOnly>
         {() => <div className={"code-editor"}>
-            <div className={"code-editor-window"}>
+            <div
+                className={"code-editor-window"}
+                style={showOutput ? {borderRadius: ".25em .25em 0 0"} : {}}
+            >
                 <AceEditor
                     value={input}
                     mode="python"
@@ -55,22 +61,36 @@ export default function CodeEditor(props) {
                     editorProps={{$blockScrolling: true}}
                     setOptions={editorOptions}
                 />
+                <div className={"button-container"}>
+                    <button
+                        className={"icon-button"}
+                        disabled={isLoading || isRunning}
+                        onClick={() => {
+                            setShowOutput(true);
+                            return runPython(input);
+                        }}
+                        aria-label={"Run Code"}
+                        title={"Run Code"}
+                    >
+                        <BsPlayFill className={"icon"} size={'1.7em'}/>
+                    </button>
+                    <button
+                        className={"icon-button"}
+                        onClick={() => setShowOutput(false)}
+                        aria-label={"Reset Code Window"}
+                        title={"Reset Code Window"}
+                    >
+                        <BiReset className={"icon"} size={"1.7em"}/>
+                    </button>
+                </div>
             </div>
-            <button
-                disabled={isLoading || isRunning}
-                onClick={() => {
-                    setShowOutput(true);
-                    return runPython(input);
-                }}>
-                Run
-            </button>
-            <button
-                onClick={() => setShowOutput(false)}>
-                Reset
-            </button>
-            {showOutput && <pre>
-                {stdout}{<span style={{color: "red"}}>{stderr}</span>}
-            </pre>}
+            {showOutput &&
+                <>
+                    <pre className={"output-window"}>
+                        {stdout}{<span style={{color: "red"}}>{stderr}</span>}
+                    </pre>
+                </>
+            }
         </div>}
     </BrowserOnly>
 }
